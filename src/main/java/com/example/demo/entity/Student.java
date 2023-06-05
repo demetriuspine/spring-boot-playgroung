d455859f10e8fb62a02f18436f4d274798722a79
lucas <mr.pine97@gmail.com>
2023-06-05 14:28:37 -0300

    cria construtor que aceita 0 parametros e builder, que facilita na instancia de objetos dessa classe

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -3,13 +3,17 @@ package com.example.demo.entity;
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
-	private Long id;
-	private String name;
-	private String email;
-	private LocalDate birthDate;
+  private Long id;
+  private String name;
+  private String email;
+  private LocalDate birthDate;
 }
