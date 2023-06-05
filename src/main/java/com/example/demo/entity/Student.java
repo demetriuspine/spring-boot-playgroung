e0dae59e444491177de9b97f5ba09cc9e2005dbe
lucas <mr.pine97@gmail.com>
2023-06-05 14:22:45 -0300

    adiciona propriedades da entidade estudante

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+public class Student {
+	private Long id;
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+}
