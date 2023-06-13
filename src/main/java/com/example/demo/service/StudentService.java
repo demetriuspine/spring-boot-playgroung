b113312a21a759eafd158a5bc2a609f9861a8510
lucas <mr.pine97@gmail.com>
2023-06-13 14:42:22 -0300

    refatora service para se comunicar com o banco de dados

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,63 +1,51 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class StudentService {
-	private static Map<Long, Student> studentsList = new HashMap<>();
+	private StudentRepository studentRepository;
 	
 	public ResponseEntity<Student> getStudentById(Long id) {
-		Student student = studentsList.get(id);
-		
-		if (student == null) {
+		if (!studentRepository.existsById(id)) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
 		
-		return ResponseEntity.status(HttpStatus.OK).body(student);
+		return ResponseEntity.status(HttpStatus.OK).body(studentRepository.findById(id).get());
 	}
 	
 	public ResponseEntity<List<Student>> getStudents() {
-		List<Student> students = new ArrayList<>(studentsList.values());
-		
-		return ResponseEntity.status(HttpStatus.OK).body(students);
+		return ResponseEntity.status(HttpStatus.OK).body(studentRepository.findAll());
 	}
 	
 	public ResponseEntity<Student> addStudent(Student newStudent) {
-		Student student = studentsList.get(newStudent.getId());
-		if (student != null) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
-		
-		studentsList.put(newStudent.getId(), newStudent);
-		return ResponseEntity.status(HttpStatus.CREATED).body(newStudent);
+		Student createdStudent = studentRepository.save(newStudent);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdStudent);
 	}
 	
-	public ResponseEntity<Student> updateStudent(Student newStudent) {
-		Student student = studentsList.get(newStudent.getId());
-		if (student == null) {
+	public ResponseEntity<Student> updateStudent(Long id, Student newStudent) {
+		if (!studentRepository.existsById(id)) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
 		
-		studentsList.put(newStudent.getId(), newStudent);
-		return ResponseEntity.status(HttpStatus.OK).body(newStudent);
+		Student updatedStudent = studentRepository.save(newStudent);
+		return ResponseEntity.status(HttpStatus.OK).body(updatedStudent);
 	}
 	
 	public ResponseEntity<String> deleteStudent(Long id) {
-		Student student = studentsList.get(id);
-		if (student == null) {
+		if (!studentRepository.existsById(id)) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		}
-		
-		studentsList.remove(id);
+		studentRepository.deleteById(id);
 		return ResponseEntity.status(HttpStatus.OK).body("Student succesfully deleted");
 	}
 }
